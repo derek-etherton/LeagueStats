30e5db1a84556efbb4c305b841da94ed283ea475
derek-etherton <derek.etherton@mail.utoronto.ca>
2020-10-19 11:02:28 -0700

    Created endpoints & methods for retrieving match list by user

--- routes/api.js
+++ routes/api.js
@@ -20,17 +20,53 @@ router.get('/', function(req, res, next) {
  * revisionDate
  * summonerLevel
  */
-router.get("/user/:username", function(req, res, next) {
+router.get('/user/:username', function(req, res, next) {
     const username = req.params.username;
-  
+    getUser(username, (data) => {
+        res.send(data);
+    });
+});
+
+router.get('/user/:username/matchlist', function(req, res, next) {
+    const username = req.params.username;
+    getMatchListByUsername(username, (data) => {
+        res.send(data);
+    });
+});
+
+function getMatchListByUsername(username, callback) {
+    getUser(username, (data) => {
+        const accountId = data.accountId;
+        if (!accountId) { console.error("No accountId found"); callback(null); }
+
+        getMatchList(data.accountId, (matchList) => {
+            callback(matchList);
+        });
+    });
+}
+
+function getMatchList(accountId, callback){
+    leagueJs.Match
+        .gettingListByAccount(accountId)
+        .then(data => {
+            callback(data);
+        })
+        .catch(err => {
+            console.error(err);
+            callback(err);
+        });
+}
+
+function getUser(username, callback){
     leagueJs.Summoner
-      .gettingByName(username)
-      .then(data => {
-          res.send(data);
-      })
-      .catch(err => {
-          console.log(err);
-      });
-  });
+        .gettingByName(username)
+        .then(data => {
+            callback(data);
+        })
+        .catch(err => {
+            console.error(err);
+            callback(err);
+        });
+}
 
 module.exports = router;
